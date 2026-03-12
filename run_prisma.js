d27e15c53d5d3a0f1f0ea65f18eb998137b2dbb6
rifqiishak <rifqiishak23@gmail.com>
2026-03-13 05:31:50 +0700

    chore: remove prisma and remaining sqlite dependencies to fix Vercel SqliteError

--- run_prisma.js
+++
@@ -1,8 +0,0 @@
-const { execSync } = require('child_process');
-try {
-  const out = execSync('npx prisma db push', { encoding: 'utf8', env: {...process.env, DATABASE_URL: "file:./dev.db"} });
-  console.log("SUCCESS:\\n", out);
-} catch (e) {
-  console.error("ERROR STDOUT:\\n", e.stdout);
-  console.error("ERROR STDERR:\\n", e.stderr);
-}
